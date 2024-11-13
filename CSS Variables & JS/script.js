447ab0f9a7b11fe9f5ff5ef46b6dd85dd8b79d56
Pragya <mi.pragya16595@gmail.com>
2024-11-13 22:13:16 +0530

    Update CSS Variables with JS

---
+++ CSS Variables & JS/script.js
@@ -0,0 +1,16 @@
+const view = {
+  addEventListener() {
+    const inputs = document.querySelectorAll('.controls input')
+    inputs.forEach(input => {
+      input.addEventListener('mousemove', this.handleInput)
+      input.addEventListener('change', this.handleInput)
+    })
+  },
+
+  handleInput() {
+    const suffix = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+  },
+}
+
+view.addEventListener();
