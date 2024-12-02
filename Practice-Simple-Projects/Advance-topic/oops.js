2a42b5ffbb8f0553964af4aa4ada9802e1cdc38a
Pragya <mi.pragya16595@gmail.com>
2024-12-02 13:56:16 +0530

    Override the values

--- Practice-Simple-Projects/Advance-topic/oops.js
+++ Practice-Simple-Projects/Advance-topic/oops.js
@@ -11,3 +11,17 @@ const user = {
 
 console.log(user.username);
 console.log(user.getUserDetails());
+
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+  this.greeting = function() {
+    console.log(`Welcome ${this.username}`);
+  }
+  // return this;
+}
+
+const user1 = new User("Saitama", 12, true);
+const user2 = new User("Eren", 11, false);
+console.log(user1);
